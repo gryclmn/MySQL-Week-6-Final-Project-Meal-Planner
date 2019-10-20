f10a405429edf1c483dfb9b06afd2a4b15f50d0b
Gary Coleman <gryclmn@hotmail.com>
2019-10-20 14:34:18 -0700

    added dish dao (#6)

--- src/dao/DishDao.java
+++ src/dao/DishDao.java
@@ -2,16 +2,50 @@ package dao;
 
 import entity.Dish;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DishDao {
+	
+	private Connection connection;
+	private final String GET_DISH_QUERY = "select * from dish";
+	private final String CREATE_DISH_QUERY = "insert into dish(dish_name, dish_type) VAUES(?, ?)";
+	private final String UPDATE_DISH_QUERY = "update dish(dish_name, dish_type) VALUES(?, ?) WHERE id = ?";
+	private final String DELETE_DISH_QUERY = "DELETE from dish WHERE id = ?";
+	
+	public DishDao(){
+		connection = DBConnection.getConnection();
+	}
 
-    public List<Dish> getAllDishes() {
-        // TODO
+    public List<Dish> getAllDishes() throws SQLException {
+    	ResultSet rs = connection.prepareStatement(GET_DISH_QUERY).executeQuery();
         List<Dish> dishList = new ArrayList<>();
+        
+        while (rs.next()) {
+        	dishList.add(new Dish(rs.getInt(1), rs.getString(2)));
+        }
 
         return dishList;
     }
-
+    public void CreateNewDish(String dishName, String dishType) throws SQLException {
+    	PreparedStatement ps = connection.prepareStatement(CREATE_DISH_QUERY);
+    	ps.setString(1, dishName);
+    	ps.setString(2, dishType);
+    	ps.executeUpdate();
+    }
+    public void UpdateDish(String dishName, String dishType) throws SQLException{
+    	PreparedStatement ps = connection.prepareStatement(UPDATE_DISH_QUERY);
+    	ps.setString(1, dishName);
+    	ps.setString(2, dishType);
+    	ps.executeUpdate();
+    }
+    public void DeleteDish(int id) throws SQLException {
+    	PreparedStatement ps = connection.prepareStatement(DELETE_DISH_QUERY);
+    	ps.setInt(1, id);
+    	ps.executeUpdate();
+    }
 }
