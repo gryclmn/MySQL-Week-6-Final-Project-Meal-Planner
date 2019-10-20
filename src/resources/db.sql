e20696e4ba3cb8191cebda427ca07e643d79b311
Gary Coleman <gryclmn@hotmail.com>
2019-10-20 10:02:22 -0700

    Updated SQL file (#4)
    * Added to gitignore.
    
    * Updated database url.
    
    * Removing all files in .gitignore
    
    * Refactored project due to time constraints. Replaced Plan class/dao with StatTracker.
    
    * Updated sql file.

--- src/resources/db.sql
+++ src/resources/db.sql
@@ -2,21 +2,18 @@ CREATE database IF NOT exists meals;
 use meals;
 
 drop table if exists meal_dish;
+drop table if exists dish_stats;
 drop table if exists dish;
-drop table if exists plan;
 drop table if exists meal;
 
 create table meal(
 	id int(10) not null auto_increment,
-	main_dish_id int(10) not null,
-	side_dish_id int(10) not null, 
-	meal_time date not null,
 	primary key(id)
 );
 create table dish(
 	id int(10) not null auto_increment, 
 	dish_name varchar(20) not null,
-	type varchar(20) not null,
+	dish_type varchar(20) not null,
 	primary key(id)
 );
 create table meal_dish(
@@ -25,11 +22,9 @@ create table meal_dish(
 	foreign key(meal_id) references meal(id),
 	foreign key(dish_id) references dish(id) 
 );
-create table plan(
-	id int(10) not null auto_increment,
-	meal_id int(10) not null,
-	schedule_start int(4),
-	schedule_end int(4),
-	primary key(id),
-	foreign key(meal_id) references meal(id)	
+CREATE TABLE dish_stats(
+	id int(10) NOT NULL,
+	dish_id int(10) NOT NULL,
+	count int(10) NOT NULL,
+	FOREIGN key(dish_id) REFERENCES dish(id)
 );
