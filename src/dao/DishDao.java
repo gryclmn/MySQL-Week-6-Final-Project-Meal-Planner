da2d304edf1572c0bbbc7e1b01e1d7c368a80b5d
Gary Coleman <gryclmn@hotmail.com>
2019-10-20 21:20:53 -0700

    Added stat-tracking to dish creation.

--- src/dao/DishDao.java
+++ src/dao/DishDao.java
@@ -13,10 +13,12 @@ public class DishDao {
 	
 	private Connection connection;
 	private final String GET_DISH_QUERY = "select * from dish";
-	private final String CREATE_DISH_QUERY = "insert into dish(dish_name, dish_type) VAUES(?, ?)";
-	private final String UPDATE_DISH_QUERY = "update dish(dish_name, dish_type) VALUES(?, ?) WHERE id = ?";
+	private final String CREATE_DISH_QUERY = "insert into dish (dish_name, dish_type) VALUES(?, ?)";
+	private final String UPDATE_DISH_QUERY = "update dish (dish_name, dish_type) VALUES(?, ?) WHERE id = ?";
 	private final String DELETE_DISH_QUERY = "DELETE from dish WHERE id = ?";
 	private final String GET_DISH_BY_ID_QUERY = "select * from dish where id = ?";
+	private final String GET_DISH_BY_NAME = "SELECT id FROM dish WHERE dish_name = ?";
+	private final String CREATE_DISH_STAT = "INSERT INTO dish_stats (dish_id, count) VALUES (?, 0)";
 	
 	public DishDao(){
 		connection = DBConnection.getConnection();
@@ -32,8 +34,7 @@ public class DishDao {
                     new Dish(
                             rs.getInt(1),
                             rs.getString(2),
-                            DishType.valueOf(rs.getString(3)
-                            )
+                            DishType.get(rs.getString(3))
                     )
             );
         }
@@ -45,14 +46,31 @@ public class DishDao {
     	ps.setInt(1, id);
     	ResultSet rs = ps.executeQuery();
     	rs.next();
-    	return new Dish(rs.getInt(1), rs.getString(2), DishType.valueOf(rs.getString(3)));
+    	return new Dish(rs.getInt(1), rs.getString(2), DishType.get(rs.getString(3)));
+    }
+    
+    private int getDishByName(String name) throws SQLException {
+    	PreparedStatement ps = connection.prepareStatement(GET_DISH_BY_NAME);
+    	ps.setString(1, name);
+    	ResultSet rs = ps.executeQuery();
+    	if (rs.next()) return rs.getInt(1);
+		return 0;
+    }
+    
+    private void createDishStat(int id) throws SQLException {
+    	PreparedStatement ps = connection.prepareStatement(CREATE_DISH_STAT);
+    	ps.setInt(1, id);
+    	ps.executeUpdate();
     }
 
     public void CreateNewDish(String dishName, String dishType) throws SQLException {
     	PreparedStatement ps = connection.prepareStatement(CREATE_DISH_QUERY);
     	ps.setString(1, dishName);
     	ps.setString(2, dishType);
+    	System.out.println(ps.toString());
     	ps.executeUpdate();
+    	
+    	createDishStat(getDishByName(dishName));
     }
     public void UpdateDish(String dishName, String dishType) throws SQLException{
     	PreparedStatement ps = connection.prepareStatement(UPDATE_DISH_QUERY);
